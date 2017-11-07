fdb8812d2fc0c63624e11f14244344ffb605b389
ray delpino <raydelpino29@gmail.com>
2017-11-07 12:35:32 -0500

    write ajax request

--- jquery_lite/lib/bundle.js
+++ jquery_lite/lib/bundle.js
@@ -68,19 +68,66 @@
 /***/ (function(module, exports, __webpack_require__) {
 
 const DOMNodeCollection = __webpack_require__(1);
+let functionsArray = [];
 
 window.$l = function (selector) {
   const elementsArray = [];
   if (selector instanceof HTMLElement) {
     elementsArray.push(selector);
     return new DOMNodeCollection(elementsArray);
-  } else {
+  } else if (typeof selector === 'string' ){
       const nodeList = document.querySelectorAll(selector);
       const nodeArray = Array.prototype.slice.call(nodeList);
       return new DOMNodeCollection(nodeArray);
+  } else if (selector instanceof Function) {
+      functionsArray.push(selector);
+  }
+  document.addEventListener("DOMContentLoaded", function () {
+    functionsArray.forEach(function(fxn) {
+      fxn();
+    });
+  });
+  if (document.readyState === "complete") {
+    functionsArray.forEach(function (fxn) {
+      fxn();
+      functionsArray = [];
+    });
   }
 };
-window.$l = $l;
+
+$l.extend = function (obj1, obj2, ...objects) {
+  let result;
+  if (true) {
+    result = Object.assign({}, obj1, obj2, ...objects);
+    return result;
+  } else {
+    result = Object.assign({}, obj1, obj2);
+    return result;
+  }
+};
+
+$l.ajax = function (options) {
+  const defaults = {
+    method: 'GET',
+    url: "",
+    success: () => {},
+    error: () => {},
+    data: {},
+    contentType: ""
+  };
+  const args = Object.assign({}, defaults, options);
+  const xhr = new XMLHttpRequest();
+  xhr.open(args.method, args.url);
+  xhr.onload = function() {
+      if (xhr.status == 200) {
+        args.success(xhr.response);
+      }
+      else {
+         args.error(xhr.response);
+      }
+   };
+   xhr.send();
+};
 
 
 /***/ }),
--- jquery_lite/lib/main.js
+++ jquery_lite/lib/main.js
@@ -1,14 +1,61 @@
 const DOMNodeCollection = require("./dom_node_collection.js");
+let functionsArray = [];
 
 window.$l = function (selector) {
   const elementsArray = [];
   if (selector instanceof HTMLElement) {
     elementsArray.push(selector);
     return new DOMNodeCollection(elementsArray);
-  } else {
+  } else if (typeof selector === 'string' ){
       const nodeList = document.querySelectorAll(selector);
       const nodeArray = Array.prototype.slice.call(nodeList);
       return new DOMNodeCollection(nodeArray);
+  } else if (selector instanceof Function) {
+      functionsArray.push(selector);
+  }
+  document.addEventListener("DOMContentLoaded", function () {
+    functionsArray.forEach(function(fxn) {
+      fxn();
+    });
+  });
+  if (document.readyState === "complete") {
+    functionsArray.forEach(function (fxn) {
+      fxn();
+      functionsArray = [];
+    });
   }
 };
-window.$l = $l;
+
+$l.extend = function (obj1, obj2, ...objects) {
+  let result;
+  if ([...objects]) {
+    result = Object.assign({}, obj1, obj2, ...objects);
+    return result;
+  } else {
+    result = Object.assign({}, obj1, obj2);
+    return result;
+  }
+};
+
+$l.ajax = function (options) {
+  const defaults = {
+    method: 'GET',
+    url: "",
+    success: () => {},
+    error: () => {},
+    data: {},
+    contentType: ""
+  };
+  const args = Object.assign({}, defaults, options);
+  const xhr = new XMLHttpRequest();
+  xhr.open(args.method, args.url);
+  xhr.onload = function() {
+      if (xhr.status == 200) {
+        args.success(xhr.response);
+      }
+      else {
+         args.error(xhr.response);
+      }
+   };
+   xhr.send();
+};
